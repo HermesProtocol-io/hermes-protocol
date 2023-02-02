5300894869d5597e6ce5228c915753e3b3a25ab8
Ana Preto <78147150+AnaFCPreto@users.noreply.github.com>
2023-02-02 20:17:05 +0000

    STG -> PRD (#18)
    * Update projects.json
    
    * Change delegation page
    
    Change delegation page for the new design
    
    * Adapt new notifications
    
    * create new function to send email
    
    * adapt new tailwind
    
    * Update .gitignore
    
    .dev.vars is a file for creating CF Worker environment variables locally while testing in a dev environment.
    
    * Change linkedin url.
    
    Update LinkedIn link... although it's not used anywhere?
    
    * Correct spelling...
    
    * Fix error in JSON
    
    * contact-form API update (still WIP)
    
    Everything works except contacting the SIB API.
    
    * create partner section
    
    * UI Improvments
    
    * Partnership page creation
    
    * Add _redirects
    
    * Update contact-form.ts
    
    * Cleaning up...
    
    * New contact form method
    
    Using Cloudflare Functions and Fetch.
    
    * Improved error handling and logging.
    
    Back-end should be fully built for the current version.
    
    Next versions can have Discord webhooks, and other API outputs....
    
    * Added HTML5 form validation
    
    Also removed google recaptcha for now.
    
    * Redo contact-form JS
    
    Still not working though...
    
    * new design homepage
    
    * delete unused images and sections
    
    * add images for the new design
    
    * adapt logo to be agnostic to colour fill
    
    * improve dark mode
    
    * add new validators
    
    * add new sections and delete the old ones from the homepage
    
    * dark mode improvement in the general design
    
    * add dark mode switch and improve the dark mode colours
    
    * design improvements
    
    * dark mode script
    
    * footer dark mode improvement
    
    * Final version of the contact form backend...
    
    * Cleaning up Form HTML
    
    * mobile adaption
    
    * mobile adaptation
    
    * dark version improvement
    
    * roadmap change
    
    * Mobile improvements
    
    * radix pop-up
    
    * dark and mobile improvements
    
    * improvements in texts
    
    * lazy load
    
    * improvement delegation
    
    * favicon colour change.
    
    * pointer in delegations
    
    * Add IDs to variables, add Radix function
    
    * Add Radix validator info to the Header
    
    * New Radix Validator Info
    
    * Rename Terra validator info
    
    So that its chain specific and not agnostic.
    Will most likely restructure once we have validators on other Cosmos chains that will most likely reuse the same code.
    
    * Replace jQuery
    
    - Removed Recaptcha reference too.
    
    * Code cleaning...
    
    * Replace old getJSON function with Fetch
    
    ---------
    
    Co-authored-by: Sérgio Rebelo <sergio@hermesprotocol.io>

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,23 +1,9 @@
-/*
- * @function getJSON
- * @description Get json content of the file
- * @param {string} URL - The url path for to getting the content
- * @returns {objectJson} 
- */
-const getJSON = URL => fetchURL(URL, 'GET', 'json');
-
-/*
- * @function fetchURL
- * @description Make ajax request based on method
- * @param {string} URL - The url path for to getting the content
- * @param {string} method - RESTful methods 
- * @param {string} dataType - Return data type as want
- * @returns {string} - The return data 
- */
-function fetchURL(URL, method, dataType) {
-    return $.ajax({
-        url: URL,
-        method: method,
-        dataType: dataType
-    });
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
 }
---
+++ functions/api/contact-form.ts
@@ -0,0 +1,144 @@
+/**
+ * POST /api/contact-form
+ */
+
+export function handleJsonRequest(r: Response, num?: number) {
+    if (r.ok) return r.json();
+    throw new Error(
+      `Error (${r.status}) with fetch! #${num??0} - '${r.statusText}' from URL: ${r.url}`
+    );
+}
+
+export function createDataBodyForUser(templateId: number, toAddress: string) {
+    return {
+        "to":[  
+            {  
+               "email": toAddress
+            }
+        ],
+        "templateId": templateId,
+        "params":{  
+            "email": toAddress
+        },
+        "headers":{
+            "charset":"iso-8859-1"
+        }
+    };
+}
+
+export function createDataBodyForStaff(
+    senderName: string,
+    senderEmail: string,
+    staffAddress: string,
+    userAddress: string,
+    replyToAddress: string,
+    subject: string,
+    userMessage: string,
+    contact_reason?: string) {
+
+        return {
+            sender: { name: senderName, email: senderEmail },
+            to:[  {  email: staffAddress } ],
+            replyTo: { name: "Staff", email: replyToAddress },
+            subject: subject,
+            textContent:
+                `From: ${userAddress}\n`+
+                `${ contact_reason? 'Subject: ' + contact_reason : ''}\n`+
+                `Message: ${userMessage}`,
+            headers:{  
+                charset: 'iso-8859-1'
+            }
+        };
+}
+
+export function simpleDataForStaff(
+    staffAddress: string,
+    subject: string,
+    userAddress: string,
+    userMessage: string,
+    contact_reason?: string) {
+
+        return createDataBodyForStaff(staffAddress, staffAddress, staffAddress, userAddress, staffAddress, subject, userMessage, contact_reason);
+}
+
+export async function onRequestPost({request, env}) {
+    let output: any = {};
+
+    try {
+        let input = await request.formData();
+        // Convert FormData to JSON
+        // NOTE: Allows multiple values per key
+        for (let [key, value] of input) {
+            let tmp = output[key];
+            if (tmp === undefined || tmp === "") {
+                output[key] = value;
+            } else {
+                output[key] = [].concat(tmp, value);
+            }
+        }
+
+        // Make sure all parameters are received.
+        if(!('email' in output && 'contact_reason' in output && 'message' in output))
+            throw new Error("Does not contain mandatory parameters.");
+
+        console.log("Valid form data received:")
+        console.log(output)
+    } catch (err) {
+        return new Response(`Error parsing JSON content.\n${err}`, { status: 400 });
+    }
+
+    try {
+        const headerData = {
+            method: 'POST',
+            headers: {
+                "Accept": "application/json",
+                "api-key": env.SENDINBLUE_API_KEY,
+                "content-type": "application/json"
+            }
+        },
+            HERMES_CONTACT_FORM_TEMPLATE = 9;
+
+        const userData = { 
+            ...structuredClone(headerData),
+            body: JSON.stringify(createDataBodyForUser(HERMES_CONTACT_FORM_TEMPLATE, output.email))
+            },
+            staffData = {
+                ...structuredClone(headerData),
+                body: JSON.stringify(simpleDataForStaff("support@hermesprotocol.io", "Hermes Protocol - Contact Form", output.email, output.message, output.contact_reason))
+            };
+
+        /*console.log(`Sending User data:`);
+        console.log(userData);
+        console.log(`Sending Staff data:`);
+        console.log(staffData);*/
+
+        const [ userResult, staffResult,  ] = await Promise.all( 
+            [
+                fetch("https://api.sendinblue.com/v3/smtp/email", userData).then((r) => handleJsonRequest(r,1)) as any,
+                fetch("https://api.sendinblue.com/v3/smtp/email", staffData).then((r) => handleJsonRequest(r,2)) as any, 
+            ]
+        );
+
+        if ("messageId" in userResult && "messageId" in staffResult) {
+            return new Response(
+                JSON.stringify({ "success": true }),
+                {
+                    status: 200,
+                    statusText: "Emails sent."
+                }
+            );
+        } else {
+            return new Response(
+                JSON.stringify({ "success": false }),
+                {
+                    status: 500,
+                    statusText: `Something unexpected happened, please try again later.`
+                }
+            );
+          }
+    } catch (err) {
+        return new Response(
+            JSON.stringify({ "success": false }),
+            { status: 400, statusText: `Problem contacting SendInBlue...\n${err}` });
+    }
+}
